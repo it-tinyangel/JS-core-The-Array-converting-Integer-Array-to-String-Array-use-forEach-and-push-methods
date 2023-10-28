c1c1ab4161c287fb573f79974e1d9999f532718f
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-10-28 16:35:09 +0300

    Convert to String Array (use `.forEach` & `String` method)

---
+++ script.js
@@ -0,0 +1,17 @@
+// The function to convert an array of numbers to an array of strings.
+function arrToString(arr) {
+	const resultStringArray = [];
+
+	arr.forEach(num => {
+		resultStringArray.push(num.toString());
+	});
+	return resultStringArray;
+}
+
+// Call the function and write the result in a variable.
+const arr1 = arrToString([1, 2, 3]);
+const arr2 = arrToString([10, 200, 3333]);
+
+// Outputing the result to the console.
+console.log(`Converted arr1 array:`, arr1);
+console.log(`Converted arr2 array:`, arr2);
